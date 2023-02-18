5d41920620932ef9dc8b1e141e61366f72535e18
laalex <laalex512@gmail.com>
2023-02-18 17:17:49 +0300

    Add additional task update3

--- ConnectToSql/main.py
+++ ConnectToSql/main.py
@@ -1,5 +1,3 @@
-# Для примера выберем выполнение 2-го задания
-
 from getpass import getpass
 from mysql.connector import connect, Error
 
@@ -10,21 +8,40 @@ try:
         password=getpass("Password:"),
     ) as connection:
         use_base = """USE homework2"""
-        task2 = """
-        SELECT
-	        id AS "id заказа",
-            CASE
-		        WHEN count_product < 100 THEN "Маленький заказ"
-                WHEN count_product BETWEEN 100 AND 300 THEN "Средний заказ"
-                ELSE "Большой заказ"
-	        END AS "Тип заказа"
-        FROM sales
-        """
+        create_table = """CREATE TABLE IF NOT EXISTS sales2
+                            (
+                            id INT PRIMARY KEY AUTO_INCREMENT,
+                            order_date DATE,
+                            count_product INT
+                            )
+                        """
+        insert_to_table = """INSERT INTO sales2 (order_date, count_product)
+                                VALUES 
+                                    ("2022-01-01", 156),
+                                    ("2022-01-02", 180),
+                                    ("2022-01-03", 21),
+                                    ("2022-01-04", 124),
+                                    ("2022-01-05", 341)
+                            """
+        read_from_table = """SELECT * FROM sales2"""
+        update_table = """UPDATE sales2 
+                            SET count_product = 0 WHERE count_product < 100
+                        """
+        drop_table = """DROP TABLE IF EXISTS sales2"""
+
         with connection.cursor() as cursor:
             cursor.execute(use_base)
-            cursor.execute(task2)
+            cursor.execute(create_table)
+            cursor.execute(insert_to_table)
+            cursor.execute(read_from_table)
             for row in cursor.fetchall():
                 print(row)
+            print("----------------------------------------------------")
+            cursor.execute(update_table)
+            cursor.execute(read_from_table)
+            for row in cursor.fetchall():
+                print(row)
+            cursor.execute(drop_table)
 except Error as e:
     print(e)
 
